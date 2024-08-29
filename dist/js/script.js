7a569837ed770617760d8559e76bbe6c9497610d
Andds2 <luismarcelodeandradejapa@hotmail.com>
2024-08-29 17:10:38 -0300

    pagination

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,32 +1,57 @@
 "use strict";
 const chx_btn = document.getElementById('chx-btn');
 const espec_infos = document.getElementById('espec-info');
-const btn_menu_resp = document.getElementById('btn-menu-responsive');
-const menu_aside = document.getElementById('menu-aside');
-const menu_responsive = document.getElementById('menu-responsive');
 let tem_espec = false;
-let menu_open = false;
 chx_btn.addEventListener('click', () => tem_espec = show_espec());
-btn_menu_resp.addEventListener('click', () => menu_open = menu_resp());
 const show_espec = () => {
     if (!tem_espec) {
-        espec_infos.style.height = 'auto';
+        espec_infos.style.height = '150px';
+        chx_btn.style.backgroundColor = 'var(--clr1)';
         return true;
     }
     else {
         espec_infos.style.height = '0';
+        chx_btn.style.backgroundColor = 'transparent';
         return false;
     }
 };
+const btn_menu_resp = document.getElementById('btn-menu-responsive');
+const menu_aside = document.getElementById('menu-aside');
+const menu_responsive = document.getElementById('menu-responsive');
+let menu_open = false;
+btn_menu_resp.addEventListener('click', () => menu_open = menu_resp());
 const menu_resp = () => {
-    if (!menu_open) {
+    if (!menu_open && window.screen.width <= 768) {
         menu_aside.style.height = '25vh';
         menu_responsive.style.display = 'flex';
         return true;
     }
-    else {
+    else if (window.screen.width <= 768) {
         menu_aside.style.height = '10vh';
         menu_responsive.style.display = 'none';
         return false;
     }
+    else {
+        return false;
+    }
+};
+const paginas = {
+    home: document.getElementById('pg-home'),
+    pedidos: document.getElementById('pg-pedidos')
+};
+const botoes = {
+    home: document.getElementById('btn-home'),
+    pedidos: document.getElementById('btn-pedidos')
+};
+botoes.home.addEventListener('click', () => { pagination('home'); if (window.screen.width <= 768)
+    document.body.style.height = '350vh'; });
+botoes.pedidos.addEventListener('click', () => { pagination('pedidos'); if (window.screen.width <= 768)
+    document.body.style.height = '150vh'; });
+const pagination = (page) => {
+    Object.keys(paginas).forEach(key => {
+        paginas[key].style.display = 'none';
+        botoes[key].classList.remove('btn-active');
+    });
+    paginas[page].style.display = 'flex';
+    botoes[page].classList.add('btn-active');
 };
--- src/script.ts
+++ src/script.ts
@@ -1,34 +1,64 @@
 const chx_btn = document.getElementById('chx-btn') as HTMLButtonElement
 const espec_infos = document.getElementById('espec-info') as HTMLDivElement
+
+let tem_espec: boolean = false
+
+chx_btn.addEventListener('click', () => tem_espec = show_espec())
+
+const show_espec = (): boolean => {
+
+    if(!tem_espec ){
+        espec_infos.style.height = '150px'
+        chx_btn.style.backgroundColor = 'var(--clr1)'
+        return true
+    } else {
+        espec_infos.style.height = '0'
+        chx_btn.style.backgroundColor = 'transparent'
+        return false
+    }
+}
+
 const btn_menu_resp = document.getElementById('btn-menu-responsive') as HTMLDivElement
 const menu_aside = document.getElementById('menu-aside') as HTMLElement
 const menu_responsive = document.getElementById('menu-responsive') as HTMLDivElement
 
-let tem_espec: boolean = false
 let menu_open: boolean = false
 
-chx_btn.addEventListener('click', () => tem_espec = show_espec())
 btn_menu_resp.addEventListener('click', () => menu_open = menu_resp())
 
-const show_espec = (): boolean => {
-
-    if(!tem_espec){
-        espec_infos.style.height = 'auto'
-        return true
-    } else {
-        espec_infos.style.height = '0'
-        return false
-    }
-}
-
 const menu_resp = ():boolean => {
-    if(!menu_open){
+    if(!menu_open && window.screen.width <= 768){
         menu_aside.style.height = '25vh'
         menu_responsive.style.display = 'flex'
         return true
-    } else {
+    } else if (window.screen.width <= 768) {
         menu_aside.style.height = '10vh'
         menu_responsive.style.display = 'none'
         return false
+    } else {
+        return false
     }
 }
+
+const paginas = {
+    home: document.getElementById('pg-home') as HTMLDivElement,
+    pedidos: document.getElementById('pg-pedidos') as HTMLDivElement
+}
+
+const botoes = {
+    home: document.getElementById('btn-home') as HTMLButtonElement,
+    pedidos: document.getElementById('btn-pedidos') as HTMLButtonElement
+}
+
+botoes.home.addEventListener('click', () => {pagination('home'); if(window.screen.width <= 768)document.body.style.height = '350vh'});
+botoes.pedidos.addEventListener('click', () => {pagination('pedidos'); if(window.screen.width <= 768)document.body.style.height = '150vh'});
+
+const pagination = (page: keyof typeof paginas): void => {
+    Object.keys(paginas).forEach(key => {
+        paginas[key as keyof typeof paginas].style.display = 'none';
+        botoes[key as keyof typeof botoes].classList.remove('btn-active');
+    });
+
+    paginas[page].style.display = 'flex';
+    botoes[page].classList.add('btn-active');
+};
